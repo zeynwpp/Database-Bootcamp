a01a1ceaac0cb849c8541f19d25ddfa16364f7cb
Zeynep Dönmez <84280282+zeynwpp@users.noreply.github.com>
2025-03-24 13:05:10 +0300

    Update Bitirme Projesi.sql

--- Bitirme Projesi.sql
+++ Bitirme Projesi.sql
@@ -1,1 +1,10 @@
-
+--SORULAR
+--Northwind veritabanında toplam kaç tablo vardır? Bu tabloların isimlerini listeleyiniz.
+--2. JOIN Sorusu:
+--Her sipariş (Orders) için, müşterinin adı (CustomerName), çalışan adı (Employee Full Name), sipariş tarihi
+--ve gönderici şirketin adı (Shipper) ile birlikte bir liste çıkarın.
+--3. Aggregate Fonksiyon:
+--Tüm siparişlerin toplam tutarını bulun. (Order Details tablosundaki Quantity UnitPrice üzerinden
+--hesaplayınız)
+--4. Gruplama:
+--Hangi ülkeden kaç müşteri vardır?
