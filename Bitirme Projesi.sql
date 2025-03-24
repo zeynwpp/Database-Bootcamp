708ed31650afe75aa09f3cdc4b977ff2723f14e5
Zeynep Dönmez <84280282+zeynwpp@users.noreply.github.com>
2025-03-24 21:02:58 +0300

    Update Bitirme Projesi.sql

--- Bitirme Projesi.sql
+++ Bitirme Projesi.sql
@@ -7,10 +7,22 @@ FROM sys.tables;
 
 --2. JOIN Sorusu:
 --Her sipariş (Orders) için, müşterinin adı (CustomerName), çalışan adı (Employee Full Name), sipariş tarihi ve gönderici şirketin adı (Shipper) ile birlikte bir liste çıkarın.
+select o.OrderID
+,c.ContactName
+,e.FirstName + ' ' + e.LastName as FullName
+,o.OrderDate 
+,s.CompanyName
+from orders o
+inner join customers c on c.CustomerID = o.CustomerID
+inner join Employees e on e.EmployeeID = o.EmployeeID
+inner join Shippers s on s.ShipperID = o.ShipVia
+
 
 --3. Aggregate Fonksiyon:
 --Tüm siparişlerin toplam tutarını bulun. (Order Details tablosundaki Quantity UnitPrice üzerinden
 --hesaplayınız)
+
+
 --4. Gruplama:
 --Hangi ülkeden kaç müşteri vardır?
 --5.Subquery Kullanımı:
